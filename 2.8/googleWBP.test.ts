f699bfb72d818c09dc13ac07bfda684476b5d4ed
Hunter <huntergerrard7@gmail.com>
2023-11-27 20:28:09 -0700

    again

--- 2.8/googleWBP.test.ts
+++ 2.8/googleWBP.test.ts
@@ -1,26 +1,10 @@
-import {Google} from './googleBP'
-import { Driver } from 'selenium-webdriver/chrome'
-const fs = require('fs')
-const google = new Google()
+import {Google} from './baseGooglePage'; 
+const google = new Google(); 
 
-test('do a search', async () => {
-    await google.navigate()
-    await google.search('Chicago Bears')
-    let text = await google.getResults()
-    expect(text).toContain('Chicago Bears')
-    await fs.writeFile(`${__dirname}/google.png`,
-     await google.driver.takeScreenshot(), "base64",
-     (e) => {
-         if (e) console.error(e)
-         else console.log('Save Succesful')
-     }
-     )
-    fs.writeFile(`${__dirname}/test.txt`, text, (e) => {
-        if (e) console.error(e)
-        else console.log('save Succesful')
-    })
-
-})
-afterAll(async () => {
-    await google.driver.quit()
-})
+test('do search', async () => {
+    await google.navigate(); 
+    await google.search('Chicago Bears'); 
+    let text = await google.getResults(); 
+    expect(text).toContain('Chicago Bears'); 
+    await google.driver.quit(); 
+}); 
